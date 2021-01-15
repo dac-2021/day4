2092774c33b87ee975397cfe043ed32c08898cd6
koderoom <koderoom.dev@gmail.com>
2021-01-15 08:17:18 +0530

    d

--- src/demo.js
+++ src/demo.js
@@ -1,4 +1,10 @@
 const fs = require("fs");
+const Promise = require("bluebird");
+Promise.promisifyAll(fs);
 
 let f1 = fs.readFileSync(`${__dirname}/demo.js`, { encoding: "utf-8" });
 console.log(f1);
+
+fs.readFileAsync(`${__dirname}/demo.js`, {
+  encoding: "utf-8",
+}).then((data) => console.log(data));
